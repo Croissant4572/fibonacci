91bed0fe06887d708e26ff5849e4402534aa9df3
Croissant4572 <inuiryota0222@gmail.com>
2020-03-05 18:04:53 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+function fib(n){
+    if (n===0){
+        return 0;
+    }else if(n===1){
+        return 1;
+    }
+    return fib(n-1) + fib(n-2); // 関数の中で関数を呼ぶのを再帰と呼ぶ
+}
+const num = 40;
+for(let i = 1; i<=num; i++){
+    console.log(fib(i));
+}
